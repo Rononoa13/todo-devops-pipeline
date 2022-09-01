a476f1b2df3dc80b13ac9e8efa46785d6a5dbb74
Sumit Luitel <sumitluitel@Sumits-MacBook-Air.local>
2022-09-01 14:24:25 +0545

    Added SQLAlchemy config

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
+from crypt import methods
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 # 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
+db = SQLAlchemy(app)
+
+# Models
+class Todo(db.Model):
+    __tablename__="todo"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
+
+@app.route("/edit")
+def home():
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
 
 @app.route("/")
-def home():
-    return "Hello World"
-
-@app.route("/base")
-def home1():
-    return render_template('base.html')
+def list():
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
